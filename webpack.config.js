dcd822502409b295d29f09d9d8edaf33e4f67508
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-07-08 20:18:00 +0530

    Resolved polyfills error

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "zlib": require.resolve("browserify-zlib"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer"),
+      "crypto": require.resolve("crypto-browserify"),
+      "util": require.resolve("util"),
+      "assert": require.resolve("assert"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "url": require.resolve("url"),
+      "path": require.resolve("path-browserify"),
+      "fs": false,
+      "net": false,
+      "tls": false
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser'
+    })
+  ]
+}; 
